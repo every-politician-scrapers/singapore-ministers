cdcdfdd65d67d04df8908d16b98715679256f51b
Tony Bowden <tony@tmtm.com>
2022-02-12 14:06:21 +0200

    compare enwiki 13th Parliament

---
+++ sources/parliament-13/scraper.rb
@@ -0,0 +1,36 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def holder_entries
+    noko.css('.navbox-columns-table .navbox-list-with-group li')
+  end
+
+  class Officeholder < OfficeholderBase
+    def name_cell
+      noko
+    end
+
+    def itemLabel
+      name_cell.css('a/@title').map(&:text).first.to_s.gsub(/\(.*?\)/, '').tidy
+    end
+
+    def startDate
+      '2020-08-24'
+    end
+
+    def endDate
+      nil
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
